6f3e938963851d16d4b31e837688d3436a4b05f3
JohannesPoulsen <92087736+JohannesPoulsen@users.noreply.github.com>
2022-01-04 09:52:35 +0100

    Initialisering af bredde og højde

--- SimpController.java
+++ SimpController.java
@@ -1,5 +1,39 @@
 package Gorillas;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
 
 public class SimpController {
+	
+	public static boolean isInt(TextField in) { //Checks id the userinput is a integer
+		try {
+			int k = Integer.parseInt(in.getText());
+			System.out.println("hej");
+			return true;
+		}catch(NumberFormatException e) {
+			System.out.println("Error: " + in.getText() + " is not a number" );
+			return false;
+		}
+		
+	}
 
 }
